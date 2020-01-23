b730f4db60801e45fd3e5b294e23349c52f8d5a6
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-01-23 03:23:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ function saturdayFun(activity='roller-skate') {
 function mondayWork(activity='go to the office') {
   return `This Monday, I will ${activity}.`
 }
+
+function wrapAdjective(flair='*') {
+  return function(input='special') {
+    return `You are ${flair}${input}${flair}`;
+  }
+}
